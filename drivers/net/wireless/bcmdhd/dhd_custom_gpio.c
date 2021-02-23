20d3f797dfdffed7fd991075a60915f78f2daaf4
kimocoder <christian@aircrack-ng.org>
2021-02-23 17:40:21 +0100

    More kernel work + More supported devices * Aimed at getting this (older) driver module less, it's easier to work   with kernels that have modules inbuilt rather than loading/offloading modules (hazzle)
    * More supported adapters of different kinds (wifi, bt, serial, hackrf/airspy etc)
    
    .. Propably more I forgot while I raging down this kernel sources.

--- drivers/net/wireless/bcmdhd/dhd_custom_gpio.c
+++ drivers/net/wireless/bcmdhd/dhd_custom_gpio.c
@@ -256,12 +256,11 @@ const struct cntry_locales_custom translate_custom_table[] = {
 *  input : ISO 3166-1 country abbreviation
 *  output: customized cspec
 */
-void
 #ifdef CUSTOM_COUNTRY_CODE
-get_customized_country_code(void *adapter, char *country_iso_code,
+void get_customized_country_code(void *adapter, char *country_iso_code,
  wl_country_t *cspec, u32 flags)
 #else
-get_customized_country_code(void *adapter, char *country_iso_code, wl_country_t *cspec)
+void get_customized_country_code(void *adapter, char *country_iso_code, wl_country_t *cspec)
 #endif /* CUSTOM_COUNTRY_CODE */
 {
 #if (defined(CUSTOMER_HW) || defined(CUSTOMER_HW2)) && (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
